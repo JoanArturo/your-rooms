dc1e6bfb651d2116b59dd970cdea5fd19d61d6b3
Joan Arturo Camacho Alvarez <joan.camacho@vemontech.com>
2023-02-24 17:42:40 -0600

    Solve error undefined variable user in navbar

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -21,7 +21,7 @@
             <div class="btn-group dropdown-avatar">
                 <div class="avatar-group" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="avatar-image">
-                        {{ $user->presenter()->profilePicture() }}
+                        {{ Auth::user()->presenter()->profilePicture() }}
                     </div>
                     <img src="{{ asset('icons/arrow-down.svg') }}" alt="Arrow down icon">
                 </div>
